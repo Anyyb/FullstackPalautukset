b96209bbfbc3b62df3a2e51cb67d76c30862e1bb
Anyyb <anusalminen1@windowslive.com>
2024-06-08 15:26:25 +0300

    puhelinluettelo step4 ja step5
    2.9*: puhelinluettelo step4
    2.10: puhelinluettelo step5

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number:'040-1231244'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   {/* Tapahtumankäsittelijä lomakkeen käsittelylle,
    jotta molemmat muutokset ppäivittyvät persons tilan listaan samalla formilla. */}
@@ -52,47 +56,88 @@ const App = () => {
     const check = persons.find(({ number }) => number === newNumber)
     console.log(check)
     if (check) {
-      alert(`${newNumber} löytyy jo listalta`);
+      alert(`${newNumber} löytyy jo listalta`)
     } else {
-    setPersons(persons.concat(numbers));
+    setPersons(persons.concat(numbers))
     setNewName('')
     setNewNumber('')
   }
 }
+{/* Käsittelee suoritettavan haun ja päivittää sen search tilaan */}
+const handleSearch = (event) => {
+  const query = event.target.value;
+  setSearch(query);
+}
+
+{/* muuttuja peopleToSow tallettaa näytettävien nimien listan search tilaan,
+  riippuen suoritetusta haun tuloksesta.*/}
+const peopleToShow = search
+  ? persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase()))
+  : persons;
+
 
   return (
     <div>
+      
       <h2>Phonebook</h2>
-      {/* Lomakkeen onSubmit saa tapahtumankäsittelijän nimen ja numeron lisäykselle */}
+      <SearchField search={handleSearch} />
+      <Formfield handleSubmit={handleSubmit} newName={newName}
+      handleName={handleName} newNumber={newNumber} handleNumber={handleNumber}/>
+      <h2>Numbers</h2>
+      <PhonebookList peopleToShow={peopleToShow}/>
+    </div>
+  )
+}
+
+ {/* Refaktoroidut komponentit renderöimistä varten */}
+
+const SearchField = (props) => {
+  return (
+    <div>
+      <div>
+      Search: <input onChange={props.search} />
+      </div>  
+    </div>
+  )
+}
+const Formfield = (props) => {
+  return (
+    <div>
+       {/* Lomakkeen onSubmit saa tapahtumankäsittelijän nimen ja numeron lisäykselle */}
       {/* Value saa oletusarvokseen newNamen eli input kentän oletusarvo */}
       {/* onChange saa tapahtumankäsittelijän syötteen käsittelylle */}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={props.handleSubmit}>
         <div>
         name: <input 
-        value={newName}
-        onChange={handleName} 
-         />
-         <div>number: <input 
-        value={newNumber}
-        onChange={handleNumber} 
-         />
-         </div>
+        value={props.newName}
+        onChange={props.handleName} 
+        />
+        <div>
+        number: <input 
+        value={props.newNumber}
+        onChange={props.handleNumber} 
+        />
+        </div>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+    </div>
+  )
+}
+const PhonebookList = (props) => {
+  return (
+    <div>
       {/*Käytetään map metodia persons listalla uuden nimen näkymän näyttämiseen.
        Nimi toimii uniikkina Id:nä. */}
       <ul>
-      {persons.map(person => (
+      {props.peopleToShow.map(person => (
       <li key={person.name}>{person.name}: {person.number}</li>
       ))}
     </ul>
     </div>
   )
-
 }
-
 export default App
