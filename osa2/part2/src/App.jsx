2db508a79411f7e024b3619e78057465d67a84fe
Anyyb <anusalminen1@windowslive.com>
2024-06-08 20:09:19 +0300

    Puhelinluettelo step6
    2.11: puhelinluettelo step6 tehtävä:
    effect hookilla ja  Axios-kirjaston avulla haetaan palvelimelta alkutila

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+   {/* Haetaan dataa palvelimelta axios.get hakee tiedot palvelimelta (palvelimen oisoite) 
+   jos yhteys onnistuu ja palvelin on käynnissä promise on fulfilled ja haettu data 
+   asetetaan renderöitäväksi app-komponentin persons tilaan. */}
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons') 
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   {/* Tapahtumankäsittelijä lomakkeen käsittelylle,
    jotta molemmat muutokset ppäivittyvät persons tilan listaan samalla formilla. */}
