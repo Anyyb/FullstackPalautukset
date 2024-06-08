98d5eb7a804154eed6baa1a71d1bd82602771797
Anyyb <anusalminen1@windowslive.com>
2024-06-08 11:45:25 +0300

    Puhelinluettelo step1 ja step 2 ja kurssitiedotApp omaksi jsx
    2.6: puhelinluettelo step1
    2.7: puhelinluettelo step2
    ja kurssitiedot tehtävä omaksi jsx talteen.

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -1,54 +1,62 @@
-import Course from './Course'
+import { useState } from 'react'
+
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+ {/* Tapahtumankäsittelijä lomakkeen käsittelylle,
+   jotta muutokset ppäivittyvät persons tilan listaan. */}
+  const handleSubmit = (event) => {
+    console.log('Uusi nimi käsitelty')
+    setNewName(event.target.value)
+  }
+ {/* Nimen lisäyksen tapahtumankäsittelijä */}
+  const addNewName= (event) => {
+    {/* Lomakkeen oletusarvoinen toiminta estetty */}
+    event.preventDefault()
+    console.log('Uusi nimi lisätty')
+    const names = {
+     name: newName}
+    const check = persons.find(({ name }) => name === newName);
+    console.log(check)
+    if(check){
+      alert(`${newName} löytyy jo listalta`)
+    } else {
+    setPersons(persons.concat(names))
+    setNewName('')
+  }
+}
+
   return (
     <div>
-      <Course course={courses} />
+      <h2>Phonebook</h2>
+      {/* Lomakkeen onSubmit saa tapahtumankäsittelijän nimen lisäykselle */}
+      {/* Value saa oletusarvokseen newNamen eli input kentän oletusarvo */}
+      {/* onChange saa tapahtumankäsittelijän syötteen käsittelylle */}
+      <form onSubmit={addNewName}>
+        <div>
+        name: <input 
+        value={newName}
+        onChange={handleSubmit} 
+         />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/*Käytetään map metodia persons listalla uuden nimen näkymän näyttämiseen.
+       Nimi toimii uniikkina Id:nä. */}
+      <ul>
+      {persons.map(person => (
+      <li key={person.name}>{person.name}</li>
+      ))}
+    </ul>
     </div>
   )
+
 }
 
 export default App
