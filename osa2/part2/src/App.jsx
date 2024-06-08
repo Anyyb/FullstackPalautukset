9b451fe14e33a60e9831ba352c4d24b7ecf3fbd2
Anyyb <anusalminen1@windowslive.com>
2024-06-08 13:12:07 +0300

    Puhelinluettelo step3
    2.8: puhelinluettelo step3

--- osa2/part2/src/App.jsx
+++ osa2/part2/src/App.jsx
@@ -2,24 +2,38 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number:'040-1231244'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
- {/* Tapahtumankäsittelijä lomakkeen käsittelylle,
-   jotta muutokset ppäivittyvät persons tilan listaan. */}
+  {/* Tapahtumankäsittelijä lomakkeen käsittelylle,
+   jotta molemmat muutokset ppäivittyvät persons tilan listaan samalla formilla. */}
   const handleSubmit = (event) => {
-    console.log('Uusi nimi käsitelty')
+    addNewName(event)
+    addNewNumber(event)
+  }
+
+ {/* Tapahtumankäsittelijät lomakkeen uuden nimen käsittelylle ja numeron,
+   jotta muutokset päivittyvät persons tilan listaan. */}
+  const handleName = (event) => {
+    console.log('Uusi nimi ja numero käsitelty')
     setNewName(event.target.value)
   }
+  const handleNumber = (event) => {
+    console.log('Uusi nimi ja numero käsitelty')
+    setNewNumber(event.target.value)
+  }
+
  {/* Nimen lisäyksen tapahtumankäsittelijä */}
   const addNewName= (event) => {
     {/* Lomakkeen oletusarvoinen toiminta estetty */}
     event.preventDefault()
     console.log('Uusi nimi lisätty')
     const names = {
-     name: newName}
-    const check = persons.find(({ name }) => name === newName);
+     name: newName,
+     number: ''}
+    const check = persons.find(({ name }) => name === newName)
     console.log(check)
     if(check){
       alert(`${newName} löytyy jo listalta`)
@@ -28,19 +42,41 @@ const App = () => {
     setNewName('')
   }
 }
+ {/* Numeron lisäyksen tapahtumankäsittelijä */}
+  const addNewNumber = (event) => {
+    event.preventDefault();
+    console.log('Uusi numero lisätty')
+    const numbers = {
+     name: newName,
+     number: newNumber }
+    const check = persons.find(({ number }) => number === newNumber)
+    console.log(check)
+    if (check) {
+      alert(`${newNumber} löytyy jo listalta`);
+    } else {
+    setPersons(persons.concat(numbers));
+    setNewName('')
+    setNewNumber('')
+  }
+}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      {/* Lomakkeen onSubmit saa tapahtumankäsittelijän nimen lisäykselle */}
+      {/* Lomakkeen onSubmit saa tapahtumankäsittelijän nimen ja numeron lisäykselle */}
       {/* Value saa oletusarvokseen newNamen eli input kentän oletusarvo */}
       {/* onChange saa tapahtumankäsittelijän syötteen käsittelylle */}
-      <form onSubmit={addNewName}>
+      <form onSubmit={handleSubmit}>
         <div>
         name: <input 
         value={newName}
-        onChange={handleSubmit} 
+        onChange={handleName} 
          />
+         <div>number: <input 
+        value={newNumber}
+        onChange={handleNumber} 
+         />
+         </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -51,7 +87,7 @@ const App = () => {
        Nimi toimii uniikkina Id:nä. */}
       <ul>
       {persons.map(person => (
-      <li key={person.name}>{person.name}</li>
+      <li key={person.name}>{person.name}: {person.number}</li>
       ))}
     </ul>
     </div>
